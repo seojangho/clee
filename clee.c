fceb9e96ebf7b18d8790c09c626e483682a32521
JangHo Seo <jangho@jangho.kr>
2015-10-18 05:44:07 +0900

    focus on syscall tracing

--- clee.c
+++ clee.c
@@ -1,7 +1,6 @@
 #include "clee.h"
 
 static _Bool tracing;
-static pid_t tracee_pgid;
 
 void clee_init() {
     tracing = false;
@@ -23,11 +22,6 @@ pid_t clee_start(const char *filename, char *const argv[], char *const envp[]) {
             /* error */
             CLEE_ERROR;
         case 0:
-            /*
-            if (setpgid(0, 0) == -1) {
-                _exit(1);
-            }
-            */
             if (ptrace(PTRACE_TRACEME, NULL, NULL, NULL) == -1)
             {
                 /* ptrace error */
@@ -36,9 +30,8 @@ pid_t clee_start(const char *filename, char *const argv[], char *const envp[]) {
             execve(filename, argv, envp);   // causes SIGTRAP
             /* execve error */
             _exit(1);
-        default:
+        default: ;
             /* parent */
-            tracee_pgid = pid;
             int status;
             if (waitpid(pid, &status, 0) == -1)
             {
@@ -47,6 +40,7 @@ pid_t clee_start(const char *filename, char *const argv[], char *const envp[]) {
             }
             if (WIFSTOPPED(status)) {
                 long const options =
+                    PTRACE_O_TRACESYSGOOD |
                     PTRACE_O_TRACEFORK |
                     PTRACE_O_TRACEVFORK |
                     PTRACE_O_TRACECLONE;
@@ -73,12 +67,10 @@ void clee_main() {
     }
     pid_t pid;
     int status;
-    while ((pid = waitpid(-1, &status, WCONTINUED|__WALL)) > 0) {
-        enum __ptrace_request behavior;
+    while ((pid = waitpid(-1, &status, __WALL)) > 0) {
         int sig2send = 0;
         if (WIFSTOPPED(status)) {
             int const cause = WSTOPSIG(status);
-            behavior = PTRACE_CONT;
             if (cause != SIGTRAP) {
                 sig2send = cause;
             }
@@ -93,7 +85,7 @@ void clee_main() {
 
         /* continue */
         if (WIFSTOPPED(status)) {
-            if (ptrace(behavior, pid, NULL, sig2send) == -1) {
+            if (ptrace(PTRACE_SYSCALL, pid, NULL, sig2send) == -1) {
                 CLEE_ERROR;
             }
         }
@@ -105,7 +97,4 @@ void clee_main() {
 }
 
 void clee_signal_handler(int sig) {
-    if (sig == SIGTSTP || sig == SIGINT) {
-        // kill(-tracee_pgid, sig);
-    }
 }
